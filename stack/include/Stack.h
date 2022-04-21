575759934f4e40e6e92d024b96e5e1c7a655e6fb
Matheus Jose <matheus.amaral@hexalab.com.br>
2022-04-21 12:59:08 -0300

    Stack Implementation Finished

---
+++ stack/include/Stack.h
@@ -0,0 +1,67 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <iostream>
+
+class Stack{
+    
+    private:
+        int top;
+        int* elements;
+        int length;
+    public:
+        
+        Stack(int length) {
+            this->top = -1;
+            this->length = length;
+            this->elements = new int[length];
+        }
+
+        ~Stack() {
+            delete[] this->elements;
+        }
+
+        void push(int element) {
+
+            if (this->isFull()) throw "Stack is full";
+
+            this->top++;
+            int position = this->top % this->length;
+            this->elements[position] = element;
+        }
+
+        int pop() {
+            
+            if (this->isEmpty()) throw "Stack is empty";
+            int element = this->elements[this->top];
+            this->elements[this->top] = 0;
+            this->top--;
+            return element;
+        }
+
+        bool isEmpty() {
+
+            bool empty = this->top == -1;
+            return empty;
+        }
+
+        bool isFull() {
+
+            bool full = this->top == this->length - 1;
+            return full;
+        }
+
+        int peek() {
+            return 0;
+        }
+
+        void print() {
+            
+            int position = this->top;
+            for (int index = position; index >= 0; index--) {
+                std::cout << "[" << elements[index] << "] ";
+            }
+        }
+};
+
+#endif
--- stack/main.cpp
+++ stack/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include "include/Stack.h"
 
 int main() {
 
-    std::cout << "Add implementation for stack data structure";
+    int size = 5;
+    Stack stack(size);
+
+    stack.push(10);
+    stack.push(11);
+    stack.push(25);
+    stack.push(49);
+    stack.push(42);
+
+    stack.print();
+
     return 0;
 }
